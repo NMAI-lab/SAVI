902afa2095fa0b871adab1d1de7fc63541c7feee
Patrick Gavigan <pgavigan@gmail.com>
2019-01-14 16:24:35 -0500

    Started setting up the unit test for PerceptionSnapshot

--- saviSimulation/savi/unitTesting/PerceptionSnapshotUnitTest.java
+++ saviSimulation/savi/unitTesting/PerceptionSnapshotUnitTest.java
@@ -1,34 +1,8 @@
 package savi.unitTesting;
 
-import java.util.ArrayList;
-
-import savi.jason_processing.Perception;
-import savi.jason_processing.PerceptionSnapshot;
-
 public class PerceptionSnapshotUnitTest {
 	public static boolean unitTest(boolean verbose) {
 		boolean testOK = true;
-		
-		// Constructor
-		PerceptionSnapshot testSnapshot = new PerceptionSnapshot();
-
-		// addPerception
-		testSnapshot.addPerception(null);
-		
-		// Copy constructor
-		
-		// addPerception
-		
-		// addPerceptionsFromSnapshot
-		
-		// ArrayList<Perception> getPerceptionList
-		
-		// getLatestVersion
-		
-		// pullSimilarPerception
-		
-		// getLiterals
-		
 		return testOK;
 	}
 }
