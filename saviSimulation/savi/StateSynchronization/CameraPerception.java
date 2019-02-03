0162c40da0293d510ae9fb0bf4dcb64e7a671ed2
Unknown <pgavigan@gmail.com>
2019-02-03 17:40:11 -0500

    Updated camera perception to always have a type - known if necessary

--- saviSimulation/savi/StateSynchronization/CameraPerception.java
+++ saviSimulation/savi/StateSynchronization/CameraPerception.java
@@ -5,11 +5,11 @@ import java.util.Arrays;
 
 public class CameraPerception extends Perception {
 
-	public CameraPerception(String perceptionName, long timeStamp, double azumuth, double elevation, double range) {
-		this(perceptionName, null, timeStamp, azumuth, elevation, range);
+	public CameraPerception(String perceptionName, double timeStamp, double azumuth, double elevation, double range) {
+		this(perceptionName, new String("unknown"), timeStamp, azumuth, elevation, range);
 	}
 	
-	public CameraPerception(String perceptionName, String type, long timeStamp, double azumuth, double elevation, double range) {
+	public CameraPerception(String perceptionName, String type, double timeStamp, double azumuth, double elevation, double range) {
 		super(perceptionName, type, timeStamp, ((ArrayList<Double>) Arrays.asList(new Double[] {azumuth, elevation, range})));
 	}
 }
