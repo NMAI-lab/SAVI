fb515beb4c8ceaa3060cf767249e75b2ce156d95
Cristina Ruiz <cruizm.89@gmail.com>
2019-02-17 19:50:57 -0500

    Going back to the original path. It was a change in my configuration

--- saviSimulation/savi/jason_processing/SAVIWorld_model.java
+++ saviSimulation/savi/jason_processing/SAVIWorld_model.java
@@ -80,7 +80,7 @@ public void setup() {
 	/***LOAD SIM PARAMETERS ****/
 	try {
 		String filePath = new File("").getAbsolutePath();
-		filePath = filePath + "/saviSimulation/config.cfg";
+		filePath = filePath + "/config.cfg";
 		System.out.println(filePath);
 		File inFile = new File(filePath);
 		in = new FileInputStream(inFile);
