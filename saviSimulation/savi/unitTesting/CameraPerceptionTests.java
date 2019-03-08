c1b71fdbd5dc6600ac8ced290030551a7f5ce289
Patrick Gavigan <pgavigan@gmail.com>
2019-03-08 12:26:34 -0500

    Bug fixes

--- saviSimulation/savi/unitTesting/CameraPerceptionTests.java
+++ saviSimulation/savi/unitTesting/CameraPerceptionTests.java
@@ -11,39 +11,6 @@ import savi.StateSynchronization.TimePerception;
 
 class CameraPerceptionTests {
 
-	
-	@Test
-	void testAngles() {
-		PVector myPosition = new PVector(100,100, 10);
-		PVector Obj1Position = new PVector(100,0, 110);
-		double mycompass = 0;
-		
-
-		//calculate distance
-		double dist  = 	myPosition.dist(Obj1Position);//Math.sqrt(deltax*deltax + deltay*deltay);
-		
-		PVector myXY = new PVector(myPosition.x,myPosition.y, 0);
-		PVector objXY = new PVector(Obj1Position.x,Obj1Position.y, 0);
-		
-		double theta1 = objXY.sub(myXY).heading();   // Math.atan2(deltay, deltax);
-		double azimuth = theta1 - (mycompass + Math.PI*0.5);// % 2* Math.PI; //(adjust to 0, 2pi) interval
-		// to normalize between 0 to 2 Pi
-		if(azimuth<0) 
-			azimuth+=2*Math.PI;
-		//if(angle>2*Math.PI) angle-=2*Math.PI;
-		
-		PVector difference = Obj1Position.copy().sub(myPosition); 
-		
-		PVector projection = new PVector(difference.x,difference.z, 0);
-		
-		
-		double elevation = PVector.angleBetween(projection, difference);   // Math.atan2(deltay, deltax);
-		//double elevation = theta2 - (mycompass + Math.PI*0.5);
-		assertEquals(Math.PI,azimuth, 0.001);
-		assertEquals(Math.PI*0.25,elevation, 0.001);
-		
-	}
-	
 	@Test
 	void test() {
 		// Make a simple perception to start
@@ -60,9 +27,9 @@ class CameraPerceptionTests {
 		testCaseLost.perceptionLost();
 		
 		// Make expected String version of the perception
-		String expected = new String("threat(2,3,4,1,unknown)");
-		String expectedWithType = new String("threat(2,3,4,1,car)");
-		String lostExpected = new String("threatlost(2,3,4,1,unknown)");
+		String expected = new String("threat(2,3,4,0,1,unknown)");
+		String expectedWithType = new String("threat(2,3,4,0,1,car)");
+		String lostExpected = new String("threatlost(2,3,4,0,1,unknown)");
 		
 		// Test toString and Literal methods
 		assertTrue(expected.equals(testCase.toString()));
