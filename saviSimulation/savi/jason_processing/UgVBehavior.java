725519425be336a565468e16c79a7aeb06297a6c
Cristina Ruiz Martin <cruizm@users.noreply.github.com>
2019-03-22 15:20:38 -0400

    Removing system outprint

--- saviSimulation/savi/jason_processing/UgVBehavior.java
+++ saviSimulation/savi/jason_processing/UgVBehavior.java
@@ -56,11 +56,8 @@ public class UgVBehavior extends UxVBehavior {
 		//Calculate new position
 		double cosv = Math.cos(this.compasAngle);
 		double sinv = Math.sin(this.compasAngle);
-		System.out.println(ugv.getPosition().toString());
 		PVector newpos = new PVector((float)(cosv*this.speedVal*timeElapsed), (float)(sinv*this.speedVal*timeElapsed), 0);
 		ugv.setPosition(ugv.getPosition().add(newpos));
-
-		System.out.println(ugv.getPosition().toString());
 		//Calculate visible items
 		this.visibleItems = new ArrayList<CameraPerception>();
 		
