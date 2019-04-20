aebab7e89882e37e64329ac3735b6a2e463acea9
Unknown <pgavigan@gmail.com>
2019-04-20 16:32:56 -0400

    Cleaned up gui

--- saviSimulation/savi/commandStation/FancyCommandStationGUI.java
+++ saviSimulation/savi/commandStation/FancyCommandStationGUI.java
@@ -1,16 +1,12 @@
 package savi.commandStation;
 
-import java.awt.BorderLayout;
-import java.awt.ComponentOrientation;
-import java.awt.Container;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.*;
 
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -25,110 +21,33 @@ public class FancyCommandStationGUI implements ActionListener {
 	private static int MAXLINES = 100;
 
 	private JFrame frame;
-	//private JTextField commands;
+	private JTextField period;
 	private JTextArea fromWifi;
 
-	//private CommandStationConnector connector; // connection to the rest of the simulation
-	//private String id; // identifier to use in messages
-
 	public FancyCommandStationGUI(CommandStationCore commandStation) {
-		//this.id = ID;
-		//this.connector = connector;
 		this.commandStation = commandStation;
 		createAndShowGUI();
 	}
 
-	private void createAndShowGUI() {
-
-		// Create and set up the window.
-		frame = new JFrame("ConsoleDemo");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		addComponentsToPane(frame); 
-
-		// create a label to display text
-//		fromWifi = new JTextArea(10, 40);
-//		JScrollPane scrollPane = new JScrollPane(fromWifi);
-//		fromWifi.setEditable(false);
-
-		// create the followTarget button
-//		JButton followTargetButton = new JButton("followTarget");
-//		followTargetButton.setSize(50, 20);
-
-		// create the achieve button
-		//JButton button = new JButton("achieve");
-		//button.setSize(50, 20);
-
-		// create the patrol button
-//		JButton patrolButton = new JButton("patrol");
-//		patrolButton.setSize(50, 20);
-
-		// create the patrol button
-//		JButton sendTelemetryButton = new JButton("getTelemetry");
-//		sendTelemetryButton.setSize(50, 20);
-
-
-
-		// create a object of JTextField with 16 columns
-//		commands = new JTextField(40);
-
-		// add buttons and textfield to panel
-//		frame.getContentPane().add(commands, BorderLayout.NORTH);
-//		frame.getContentPane().add(scrollPane, BorderLayout.SOUTH);
-//		//frame.getContentPane().add(button, BorderLayout.CENTER);
-//		frame.getContentPane().add(followTargetButton, BorderLayout.WEST);
-//		frame.getContentPane().add(patrolButton, BorderLayout.EAST);
-//		frame.getContentPane().add(sendTelemetryButton, BorderLayout.CENTER);
-
-		// Display the window.
-		frame.pack();
-		frame.setVisible(true);
-
-	}
-
+	
 	// if the button is pressed
 	public void actionPerformed(ActionEvent e) {
 
-		//long mid = System.currentTimeMillis() % 20000; // this is a really crude message id!
-		//String message;
-
-		// Deal with the type of button that was pressed
-		//if (e.getActionCommand().equals("achieve")) {
-
-		//	// set the text of the label to the text of the field
-		//	message = "<" + mid + "," + this.id + ",achieve,BROADCAST," + commands.getText() + ">";
-		//} else 
 		if (e.getActionCommand().equals("Request single telemetry")) {
 			this.commandStation.sendMessage("BROADCAST", "achieve", "sendTelemetry");
-			//message = "<" + mid + "," + this.id + ",achieve,BROADCAST,sendTelemetry>";
-			
-			// send the message to the simulator
-			//connector.messageOut(message);
 			
 		} else if (e.getActionCommand().equals("Patrol")) {
 			this.commandStation.sendMessage("BROADCAST", "achieve", "patrol");
-			//message = "<" + mid + "," + this.id + ",achieve,BROADCAST,patrol>";
-			
-			// send the message to the simulator
-			//connector.messageOut(message);
 			
 		} else if (e.getActionCommand().equals("Follow Target")) {
 			this.commandStation.sendMessage("BROADCAST", "achieve", "followTarget");
-			//message = "<" + mid + "," + this.id + ",achieve,BROADCAST,followTarget>";
-			
-			// send the message to the simulator
-			//connector.messageOut(message);
 		} else if (e.getActionCommand().equals("Set telemetry frequency")) {
-			// Set the telemetry period (make it 1 second for now)
-			this.commandStation.setTelemetryPeriod(1000);
+			// Set the telemetry period
+			this.setTelemetryPeriod();
 		}
-
-		
-
-		// set the text of field to blank
-		//commands.setText("  ");
-
 	}
+	
+
 
 	/**
 	 * Method to receive messages from the simulator
@@ -152,87 +71,38 @@ public class FancyCommandStationGUI implements ActionListener {
 		}
 
 	}
-
-	private void getTelemetryPanel(Container panel) {
-		panel.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
-
-		// Set up the border
-		TitledBorder title = new TitledBorder("Telemetry Settings");
-		((JComponent) panel).setBorder(title);
-
-		// Set the layout
-		panel.setLayout(new GridBagLayout());
-		GridBagConstraints c = new GridBagConstraints();
-
-		JButton button;
-		button = new JButton("Button 1");
-		c.weightx = 0.5;
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.gridx = 0;
-		c.gridy = 0;
-		panel.add(button, c);
-		button.addActionListener(this);
-
-		button = new JButton("Button 2");
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.weightx = 0.5;
-		c.gridx = 1;
-		c.gridy = 0;
-		panel.add(button, c);
-		button.addActionListener(this);
-
-		button = new JButton("Set telemetry frequency");
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.weightx = 0.5;
-		c.gridx = 2;
-		c.gridy = 0;
-		panel.add(button, c);
-		button.addActionListener(this);
-
-		button = new JButton("Request single telemetry");
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.weightx = 0.5;
-		c.gridx = 2;
-		c.gridy = 1;
-		panel.add(button, c);
-		button.addActionListener(this);
+	
+	
+	/**
+	 * Get the telemetry period from the text box and send it to the command station
+	 */
+	private void setTelemetryPeriod() {
+		int value = Integer.parseInt(period.getText());
+        period.setText(Integer.toString(value));
+        this.commandStation.setTelemetryPeriod((long)value);
 	}
 
-	private void getMissionPanel(Container panel) {
-		panel.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+	private void createAndShowGUI() {
 
-		// Set up the border
-		TitledBorder title = new TitledBorder("Load Mission");
-		((JComponent) panel).setBorder(title);
+		// Create and set up the window.
+		frame = new JFrame("ConsoleDemo");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		addComponentsToPane(frame); 
 
-		// Set the layout
-		panel.setLayout(new GridBagLayout());
-		GridBagConstraints c = new GridBagConstraints();
+		// Display the window.
+		frame.pack();
+		frame.setVisible(true);
 
-		JButton button;
-
-		button = new JButton("Patrol");
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.weightx = 0.5;
-		c.gridwidth = 3;
-		c.gridx = 0;
-		c.gridy = 0;
-		panel.add(button, c);
-		button.addActionListener(this);
-
-		button = new JButton("Follow Target");
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.weightx = 0.5;
-		c.gridwidth = 3;
-		c.gridx = 0;
-		c.gridy = 1;
-		panel.add(button, c);
-		button.addActionListener(this);
 	}
 
 	private void addComponentsToPane(Container mainPane) {
 		// Set up the panels
 		Container mainPanel = new JPanel();
+		
+		// Build the top part of the panel
+		Container top = new JPanel();
+		top.setLayout(new GridLayout(2,1));
 		Container telemetryPanel = new JPanel();
 		Container missionPanel = new JPanel();
 
@@ -240,16 +110,83 @@ public class FancyCommandStationGUI implements ActionListener {
 		getTelemetryPanel(telemetryPanel);
 		getMissionPanel(missionPanel);
 		
-		// create a label to display text
-		fromWifi = new JTextArea(10, 40);
-		JScrollPane scrollPane = new JScrollPane(fromWifi);
-		fromWifi.setEditable(false);
-
+		top.add(telemetryPanel);
+		top.add(missionPanel);
+		
+		// Build the bottom part of the panel
+		Container bottom = new JPanel();
+		this.buildMessageWindow(bottom);
+		
+		// Set the layout
+		mainPanel.setLayout(new GridLayout(2,1));
+		
 		// Add the panels to the mainPane
 		mainPane.add(mainPanel);
-		mainPanel.add(telemetryPanel, BorderLayout.NORTH);
-		mainPanel.add(missionPanel, BorderLayout.CENTER);
-		mainPanel.add(fromWifi, BorderLayout.SOUTH);
+		mainPanel.add(top);
+		mainPanel.add(bottom);
 	}
+	
+	private void getTelemetryPanel(Container panel) {
+		//panel.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+
+		// Set up the border
+		TitledBorder title = new TitledBorder("Telemetry Settings");
+		((JComponent) panel).setBorder(title);
+
+		// Set the layout
+		panel.setLayout(new GridLayout(2,2));
+		//GridBagConstraints c = new GridBagConstraints();
+
+		JLabel label = new JLabel("Telemetry period in ms (0 for no telemetry ping):");
+	    panel.add(label); 
+		
+		JButton button;
+		button = new JButton("Set telemetry frequency");
+		panel.add(button);
+		button.addActionListener(this);
+
+		// Make the text field for the period
+		period = new JTextField(); 
+		period.setText(Integer.toString(0));
+	    panel.add(period); 
+		
+		button = new JButton("Request single telemetry");
+		panel.add(button);
+		button.addActionListener(this);
+	}
+
+	private void getMissionPanel(Container panel) {
+
+		// Set up the border
+		TitledBorder title = new TitledBorder("Load Mission");
+		((JComponent) panel).setBorder(title);
+
+		// Set the layout
+		panel.setLayout(new GridLayout(2,1));
+
+		JButton button;
+
+		button = new JButton("Patrol");
+		panel.add(button);
+		button.addActionListener(this);
+
+		button = new JButton("Follow Target");
+		panel.add(button);
+		button.addActionListener(this);
+	}
+	
+	private void buildMessageWindow(Container panel) {
+		
+		// Set up the border
+		TitledBorder title = new TitledBorder("Messages");
+		((JComponent) panel).setBorder(title);
+		
+		// create a label to display text
+		fromWifi = new JTextArea(20, 40);
+		JScrollPane scrollPane = new JScrollPane(fromWifi);
+		fromWifi.setEditable(false);
+		panel.add(scrollPane);
+	}
+
 	
 }
